dbb6a8a29a8bd5be315c02d0062180ca83e60786
kimjin <kimjin@nemustech.com>
2021-11-19 20:55:50 +0900

    add commit 11

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -8,7 +8,7 @@ import android.view.Menu
 import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
-    //aabbccdd
+    //aabbccddeeee
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
